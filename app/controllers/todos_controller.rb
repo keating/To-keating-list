438138782919bbc9c5d282334e6ecebc5320dfa2
keating <outofclouds@gmail.com>
2014-05-10 22:21:28 +0800

    change sort order

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,8 +3,8 @@ class TodosController < ApplicationController
 
   def index
     @todo = Todo.new
-    @todos = Todo.active.order('id desc')
-    @todo_archived = Todo.archived.order('id desc').limit(3)
+    @todos = Todo.active.order('updated_at desc')
+    @todo_archived = Todo.archived.order('updated_at desc').limit(3)
   end
 
   def create
