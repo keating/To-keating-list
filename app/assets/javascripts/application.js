5f2401fa67ca3ed7543dbad86adb53da0a701e3b
keating <outofclouds@gmail.com>
2014-05-10 13:19:39 +0800

    drag and drop

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,24 @@
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
+
+$(function () {
+    $(".new-todo").draggable({
+//        revert: 'invalid',
+        helper: "clone",
+        cursorAt: {left: 5}
+    });
+
+    $("#finished_todos").droppable({
+        accept: ".new-todo",
+        drop: function (event, ui) {
+            var ele = $(ui.draggable);
+            var finishedTodo = $('<a href="#" class="list-group-item"></a>');
+            finishedTodo.append($('<h4 class="list-group-item-heading"></h4>').text(ele.data('title')));
+            finishedTodo.append($('<p class="list-group-item-text"></p>').text(ele.data('remark')));
+            $(this).find('.active').after(finishedTodo);
+            ele.fadeOut('fast').remove();
+        }
+    });
+
+});
