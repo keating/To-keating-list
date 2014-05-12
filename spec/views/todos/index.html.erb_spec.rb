f57ec4be7d52b011fea4668f56206b371f6c1bee
keating <outofclouds@gmail.com>
2014-05-12 17:40:20 +0800

    index page of todos

--- spec/views/todos/index.html.erb_spec.rb
+++ spec/views/todos/index.html.erb_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'todos/_signed_in.html.erb' do
+describe 'todos/index.html.erb' do
   it "there is a new todo link" do
     assign(:todo, stub_model(Todo))
     assign(:todos, [stub_model(Todo)])
